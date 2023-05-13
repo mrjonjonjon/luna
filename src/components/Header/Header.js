1c43be6d21b5e177e140823bdabe119b41a30691
Jonathan Akaba <jonathanakaba@jka.local>
2023-05-13 16:49:34 -0700

    moved images to src

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,14 @@ import BackgroundCollage from '../BackgroundCollage/BackgroundCollage.js';
 import { Outlet,Link } from 'react-router-dom';
 import './Header.css';
 import NavBar from '../NavBar/NavBar.js';
-import AnimatedBg from '../AnimatedBg/AnimatedBg.js';
-import SpiralBg from '../SpiralBackground/SpiralBg.js';
+import portrait from '../../images/7066.jpeg'
 function Header(){
     return  <>
               <NavBar/>
 
             <div className="head-container">
                 <BackgroundCollage/>
-                    <img src="images/7066.jpeg" style={{pointerEvents:'none'}} alt="luna" height="100%"/>
+                    <img src={portrait} style={{pointerEvents:'none'}} alt="luna" height="100%"/>
                 
                     <h1 className="text-overlay" style={{pointerEvents:'none'}}>ЛУНА</h1>
                     <h1 className='by-line'><li>Photographer</li> <li>Singer</li><li>Model</li></h1>
